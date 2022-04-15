aba95dfa52714c7ea1707d95b78f48322e8f1e77
Ericson Weah <ericson.weah@utah.edu>
2022-04-14 22:31:58 -0600

    README refractored

--- routes/user/index.js
+++ routes/user/index.js
@@ -13,7 +13,7 @@ const {index, store, show, edit, update, destroy} = new UsersController
 | Here is where you can register users routes for your application. These
 | routes are first mounted to the Router (see Router Class in /src/Router.js)
 | and then to the  App (See  App Class in /src/App.js)
-| contains the "web" middleware group. Now create something great!
+| Now create something great!
 |
 */
 module.exports = app => {
